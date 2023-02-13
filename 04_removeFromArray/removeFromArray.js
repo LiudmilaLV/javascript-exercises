ac44d4a6b9864c37dfad3fb6d2203cfb83e42582
LiudmilaLV <lisikhina.lv@gmail.com>
2023-02-13 16:08:42 +0700

    do 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,19 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...items) {
+    let newArr = [];
+    let noMatch = true;
+    for (let i = 0; i < (arr.length + 1); i++) {
+        noMatch = true;
+        for (let j = 0; j < (items.length + 1); j++) {
+            if (arr[i] === items[j]) {
+                noMatch = false;
+                continue;
+            }
+        }
+        if (noMatch) {
+            newArr.push(arr[i]);
+        }
+    }
+    return newArr;
 };
 
 // Do not edit below this line
